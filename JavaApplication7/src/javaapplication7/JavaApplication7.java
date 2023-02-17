cbb10579ff9d379642e6211dc8fad6c27dfbf9c8
Zura Kajaia <kajaia.zura@kiu.edu.ge>
2023-02-18 00:48:10 +0400

    Added development tools122

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -16,6 +16,7 @@ public class JavaApplication7 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD");
     }
     
 }
